db466ef69faaead35a80d2ca388cd02ccf8c2f61
Saurabh Satish Pote <saurabhpote3@gmail.com>
2024-05-14 20:55:12 +0530

    commit

--- script.js
+++ script.js
@@ -2,23 +2,36 @@ const task = [];
 
 function addTask() {
     const inputTask = document.querySelector('.task');
-    const taskValue = inputTask.value.trim(); 
-    if (taskValue !== '') { 
+    const taskValue = inputTask.value;
+    if (taskValue.trim() !== '') {
         task.push(taskValue);
-        inputTask.value = ''; 
+        inputTask.value = '';
         renderTask();
     } else {
         alert('Please enter a task!');
     }
 }
 
+function deleteTask(index) {
+    task.splice(index, 1);
+    renderTask();
+}
+
 function renderTask() {
     const taskList = document.querySelector('.showTask');
-    taskList.innerHTML = ''; 
+    taskList.innerHTML = '';
 
-    task.forEach(item => {
+    task.forEach((item, index) => {
         const li = document.createElement('li');
         li.textContent = item;
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.addEventListener('click', () => {
+            deleteTask(index);
+        });
+
+        li.appendChild(deleteButton);
         taskList.appendChild(li);
     });
 }
